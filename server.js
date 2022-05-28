7d16452033920fdb922221851b89957cbab83ab0
Kamuro-tan <>
2022-05-28 19:16:19 +0300

    End 8th lab.

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(express.static(__dirname + "/client"));
 app.use(express.urlencoded({ extended: true }));
 
 // подключаемся к хранилищу данных Amazeriffic в Mongo - ДЛЯ ЭТОГО НУЖЕН MongoDB!
-mongoose.connect('mongodb://localhost/amazeriffic', (err) => {
+mongoose.connect('mongodb://localhost/amazeriffic', function (err) {
     if (!err) console.log("Mongo connected.");
-    else console.log("Mongo connect error!");
+    else console.log("---Mongo connect Error!---", err);
 });
 
 var ToDoSchema = mongoose.Schema({
     description: String,
-    tags: [ String ]
+    tags: [String]
 });
 var ToDo = mongoose.model("ToDo", ToDoSchema);
 
@@ -23,15 +23,32 @@ http.createServer(app).listen(3000);
 
 // этот маршрут замещает наш файл todos.json в примере из части 5
 app.get("/todos.json", function (req, res) {
-    ToDo.find({}, function(err, toDos) {
+    ToDo.find({}, function (err, toDos) {
         if (!err) res.json(toDos);
-        else console.log("Get error!");
+        else console.log("---Get Error!---", err);
     });
 });
 
 app.post("/todos", function (req, res) {
-    var newToDo = req.body;
-    console.log(newToDo);
+    console.log(req.body);
+    
+    var newToDo = new ToDo({
+        "description": req.body.description,
+        "tags": req.body.tags
+    });
 
-    res.json({ "message": "Вы размещаетесь на сервере!" });
+    newToDo.save(function (err, result) {
+        if (err !== null) {
+            console.log(err);
+            res.send("---Saving Error!---");
+        } else {
+            // клиент ожидает, что будут возвращены все задачи, поэтому для сохранения совместимости сделаем дополнительный запрос
+            ToDo.find({}, function (err, result) {
+                if (err !== null) {
+                    res.send("Error! NewToDo wasn't save.");
+                }
+                res.json(result);
+            });
+        }
+    });
 });
